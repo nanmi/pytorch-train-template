386e7f7a43994d57c9439623d4851c7dd2c4a09b
nanmi <37356276+nanmi@users.noreply.github.com>
2021-09-03 13:25:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Pytorch Train Template Demo:o:
+
+This project is a template for training use Pytorch.
+
+### Train
+
+``` shell script
+CUDA_VISIBLE_DEVICES=3,4,5 python train.py
+```
+
+### Valid
+
+Set environment variables. Run:
+
+``` shell script
+CUDA_VISIBLE_DEVICES=3,4,5 python valid.py
+```
+
---
+++ eval.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torch.backends.cudnn as cudnn
+import torch.optim
+import os
+import sys
+import argparse
+import time
+import dataloader
+from model import model
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+import glob
+import time
+
+# os.environ['CUDA_VISIBLE_DEVICES']='0'
+ 
+def lowlight(image_path):
+	data_lowlight = Image.open(image_path)
+	data_lowlight = (np.asarray(data_lowlight)/255.0)
+
+	data_lowlight = torch.from_numpy(data_lowlight).float()
+	data_lowlight = data_lowlight.permute(2,0,1)
+	data_lowlight = data_lowlight.cuda().unsqueeze(0)
+
+	net = model.enhance_net_nopool().cuda()
+	net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
+	start = time.time()
+
+	_,enhanced_image,_ = net(data_lowlight)
+
+	end_time = (time.time() - start)
+	print(end_time)
+	image_path = image_path.replace('test_data','result')
+	result_path = image_path
+	if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
+		os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
+
+	torchvision.utils.save_image(enhanced_image, result_path)
+
+# test_images
+if __name__ == '__main__':
+	with torch.no_grad():
+		filePath = 'data/test_data/'
+		file_list = os.listdir(filePath)
+		for file_name in file_list:
+			test_list = glob.glob(filePath+file_name+"/*") 
+			for image in test_list:
+				lowlight(image)
+
